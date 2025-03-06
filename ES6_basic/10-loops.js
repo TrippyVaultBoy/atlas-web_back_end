8b1b8030287a870f555bdadabcd5895786a64491
Tanner Saint <tsaint2004@gmail.com>
2025-03-06 21:05:05 +0000

    fixed names

--- ES6_basic/10-loops.js
+++ ES6_basic/10-loops.js
@@ -1,7 +1,7 @@
 export default function appendToEachArrayValue(array, appendString) {
     for (let element of array) {
-      let value = array[idx];
-      array[idx] = appendString + value;
+      let value = array[element];
+      array[element] = appendString + value;
     }
 
     return array;
