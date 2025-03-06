94b413009affad3b1e56fbee469ffa575e5cb5ef
Tanner Saint <tsaint2004@gmail.com>
2025-03-06 04:38:57 +0000

    fixed function decleration

--- ES6_basic/2-arrow.js
+++ ES6_basic/2-arrow.js
@@ -1,9 +1,9 @@
-const getNeighborhoodsList = () => {
+export default function getNeighborhoodsList() {
   this.sanFranciscoNeighborhoods = ['SOMA', 'Union Square'];
 
   const self = this;
-  this.addNeighborhood = function add(newNeighborhood) {
+  this.addNeighborhood = () => {
     self.sanFranciscoNeighborhoods.push(newNeighborhood);
     return self.sanFranciscoNeighborhoods;
-    }; 
+  };
 }
