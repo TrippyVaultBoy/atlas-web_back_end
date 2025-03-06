4c740b4467c09e89a7940c69f59da8a6d9027dd1
Tanner Saint <tsaint2004@gmail.com>
2025-03-06 20:56:39 +0000

    fixed loop

--- ES6_basic/10-loops.js
+++ ES6_basic/10-loops.js
@@ -1,5 +1,5 @@
 export default function appendToEachArrayValue(array, appendString) {
-    for (const idx in array) {
+    for (let idx of array) {
       let value = array[idx];
       array[idx] = appendString + value;
     }
