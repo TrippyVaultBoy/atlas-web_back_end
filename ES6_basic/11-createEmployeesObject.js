83ac18771ede52703429d8cf4f79ee17740de56d
Tanner Saint <tsaint2004@gmail.com>
2025-03-09 23:18:05 +0000

    changed object

--- ES6_basic/11-createEmployeesObject.js
+++ ES6_basic/11-createEmployeesObject.js
@@ -1,6 +1,6 @@
 export default function createEmployeesObject(departmentName, employees) {
     const employees = {
-        $departmentName: [$employees]
+        departmentName: [employees]
     }
     return department
 }
