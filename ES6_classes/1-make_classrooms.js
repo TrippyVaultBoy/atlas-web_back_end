45c5b4eb55c17d43977fc5522479c90a9e8ad312
Tanner Saint <tsaint2004@gmail.com>
2025-03-11 06:12:32 +0000

    added 1-make_classrooms.js

---
+++ ES6_classes/1-make_classrooms.js
@@ -0,0 +1,14 @@
+export default class ClassRoom {
+  constructor(maxStudentsSize) {
+    this._maxStudentsSize = maxStudentsSize;
+  }
+
+  initializeRooms() {
+    return [
+        new ClassRoom(19),
+        new ClassRoom(20),
+        new ClassRoom(34)
+    ];
+  }
+}
+  
