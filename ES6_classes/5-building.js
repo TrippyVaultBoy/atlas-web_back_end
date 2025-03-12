aa4e0c62a51ad5317d38518f5dd554a372b15766
Tanner Saint <tsaint2004@gmail.com>
2025-03-12 21:18:54 +0000

    added warning method

--- ES6_classes/5-building.js
+++ ES6_classes/5-building.js
@@ -1,15 +1,13 @@
 export default class Building {
   constructor(sqft) {
-    if (typeof sqft !== 'number') {
-      throw new TypeError('Sqft must be a number');
-    }
-    if (new.target === Building) {
-      throw new Error("Cannot instantiate an abstract class.");
-    }
     if (typeof this.evacuationWarningMessage !== 'function') {
       throw new TypeError('Class extending Building must override evacuationWarningMessage');
     }
     this._sqft = sqft
+  }
+
+  evacuationWarningMessage() {
+    throw new Error('Class extending Building must override evacuationWarningMessage');
   }
 
   // sqft getter and setter
