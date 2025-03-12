325bcb921eaa49623a4189ce72151bcb3bb9ead1
Tanner Saint <tsaint2004@gmail.com>
2025-03-12 21:22:32 +0000

    fixed lint

--- ES6_classes/5-building.js
+++ ES6_classes/5-building.js
@@ -4,10 +4,10 @@ export default class Building {
       throw new TypeError('Class extending Building must override evacuationWarningMessage');
     }
     if (new.target === Building) {
-      throw new Error("Cannot instantiate an abstract class.");
+      throw new Error('Cannot instantiate an abstract class.');
     }
     if (this.evacuationWarningMessage === Building.prototype.evacuationWarningMessage) {
-      throw new Error("Class extending Building must override evacuationWarningMessage");
+      throw new Error('Class extending Building must override evacuationWarningMessage');
     }
     this._sqft = sqft;
   }
