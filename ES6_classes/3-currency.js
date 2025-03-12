d5ac9d807581bb6e983d9a003dc72253213802d0
Tanner Saint <tsaint2004@gmail.com>
2025-03-12 04:25:46 +0000

    fixed lint

--- ES6_classes/3-currency.js
+++ ES6_classes/3-currency.js
@@ -1,11 +1,11 @@
 export default class Currency {
   constructor(code, name) {
     if (typeof code !== 'string') {
-      new TypeError('Code must be a string');
+      TypeError('Code must be a string');
     }
 
     if (typeof name !== 'string') {
-      new TypeError('Name must be a string');
+      TypeError('Name must be a string');
     }
 
     this._code = code;
@@ -19,7 +19,7 @@ export default class Currency {
 
   set code(value) {
     if (typeof code !== 'string') {
-      new TypeError('Code must be a string');
+      TypeError('Code must be a string');
     }
 
     this._code = value;
@@ -32,7 +32,7 @@ export default class Currency {
 
   set name(value) {
     if (typeof value !== 'string') {
-      new TypeError('Code must be a string');
+      TypeError('Code must be a string');
     }
 
     this._name = value;
