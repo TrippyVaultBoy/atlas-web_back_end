3d1043f380c4cd03c0fc6a21db744abcb95b4f37
Tanner Saint <tsaint2004@gmail.com>
2025-03-12 21:11:02 +0000

    added override check

--- ES6_classes/5-building.js
+++ ES6_classes/5-building.js
@@ -3,6 +3,9 @@ export default class Building {
     if (typeof sqft !== 'number') {
       throw new TypeError('Sqft must be a number');
     }
+    if (typeof this.evacuationWarningMessage !== 'function') {
+      throw new TypeError('Class extending Building must override evacuationWarningMessage');
+    }
     this._sqft = sqft
   }
 
