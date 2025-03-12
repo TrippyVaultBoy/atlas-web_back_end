9ba9841828bdfbe3027c56a78a0e6d7a9d5e4567
Tanner Saint <tsaint2004@gmail.com>
2025-03-12 21:42:02 +0000

    fixed lint

--- ES6_classes/5-building.js
+++ ES6_classes/5-building.js
@@ -4,13 +4,13 @@ export default class Building {
       throw new TypeError('Sqft must be a number');
     }
     this._sqft = sqft;
-    if (this.constructor !== Building && 
-        this.evacuationWarningMessage === Building.prototype.evacuationWarningMessage) {
+    if (this.constructor !== Building
+        && this.evacuationWarningMessage === Building.prototype.evacuationWarningMessage) {
       throw new Error('Class extending Building must override evacuationWarningMessage');
     }
   }
 
-  evacuationWarningMessage() {
+  get evacuationWarningMessage() {
     throw new Error('Class extending Building must override evacuationWarningMessage');
   }
 
