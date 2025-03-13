2079bd344507faa9423cc7ef33f39c36d1de90b9
Tanner Saint <tsaint2004@gmail.com>
2025-03-13 02:23:17 +0000

    fixed lint

--- ES6_data_manipulation/9-groceries_list.js
+++ ES6_data_manipulation/9-groceries_list.js
@@ -1,11 +1,11 @@
 export default function groceries_list() {
-	const myMap = new Map();
+  const myMap = new Map();
 
-	myMap.set('Apples', 10);
-	myMap.set('Tomatoes', 10);
-	myMap.set('Pasta', 1);
-	myMap.set('Rice', 1);
-	myMap.set('Banana', 5);
+  myMap.set('Apples', 10);
+  myMap.set('Tomatoes', 10);
+  myMap.set('Pasta', 1);
+  myMap.set('Rice', 1);
+  myMap.set('Banana', 5);
 
-	return myMap;
+  return myMap;
 }
