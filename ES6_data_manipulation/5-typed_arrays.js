156993b7064e0cf1c75bbc79a09ab9c8f29dbc42
Tanner Saint <tsaint2004@gmail.com>
2025-03-13 01:13:25 +0000

    returns view instead of buffer

--- ES6_data_manipulation/5-typed_arrays.js
+++ ES6_data_manipulation/5-typed_arrays.js
@@ -3,7 +3,7 @@ export default function createInt8TypedArray(length, position, value) {
     throw new Error('Position outside range');
   }
   const buffer = new ArrayBuffer(length);
-  const intView = new DataView(buffer);
-  intView.setInt8(position, value);
-  return buffer;
+  const view = new DataView(buffer);
+  view.setInt8(position, value);
+  return view;
 }
