d5cd7705ab1c4240b5e641680455fd5f09680cda
Tanner Saint <tsaint2004@gmail.com>
2025-03-13 03:27:04 +0000

    fixed lint

--- ES6_promise/2-then.js
+++ ES6_promise/2-then.js
@@ -2,10 +2,10 @@ export default function handleResponseFromAPI(promise) {
   return promise
     .then(() => {
       console.log('Got a response from the API');
-      return {status: 200, body: 'success'};
+      return { status: 200, body: 'success' };
     })
     .catch(() => {
       console.log('Got a response from the API');
       return new Error();
-    })
+    });
 }
