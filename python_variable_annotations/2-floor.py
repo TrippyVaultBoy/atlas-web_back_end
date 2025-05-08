7efb7386ed3c69e7f42b179bed134ee5977c9994
REPLACE WITH YOUR NAME <REPLACE_WITH_YOUR_EMAIL@DOMAIN.COM>
2025-05-08 21:43:12 +0000

    fixed import location

--- python_variable_annotations/2-floor.py
+++ python_variable_annotations/2-floor.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
-import math
 """
 Write a type-annotated function floor
 which takes a float n as argument and
 returns the floor of the float.
 """
+import math
 
 
 def floor(n: float) -> int:
