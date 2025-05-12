ed598564ea49f9b438d317db634744884cd76d55
REPLACE WITH YOUR NAME <REPLACE_WITH_YOUR_EMAIL@DOMAIN.COM>
2025-05-12 01:39:00 +0000

    added 8-make_multiplier.py

---
+++ python_variable_annotations/8-make_multiplier.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""
+Write a type-annotated function make_multiplier
+that takes a float multiplier as argument and returns
+a function that multiplies a float by multiplier.
+"""
+from typing import Callable
+
+
+def make_multiplier(multiplier: float) -> Callable[[float], float]:
+    """
+    make_multiplier function
+    """
+    def _multiply(num: float) -> float:
+        """
+        _multiply function
+        """
+        return num * multiplier
+    
+    return _multiply
