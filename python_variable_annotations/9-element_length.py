b6fbe7f3cef8d00de17fd2af32ecf1974c3a917a
REPLACE WITH YOUR NAME <REPLACE_WITH_YOUR_EMAIL@DOMAIN.COM>
2025-05-12 01:48:10 +0000

    changed str to Sequance

--- python_variable_annotations/9-element_length.py
+++ python_variable_annotations/9-element_length.py
@@ -3,8 +3,8 @@
 Annotate the below function’s parameters and
 return values with the appropriate types
 """
-from typing import List, Tuple, Iterable
+from typing import List, Tuple, Iterable, Sequence
 
 
-def element_length(lst: Iterable[str]) -> List[Tuple[str, int]]:
+def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
     return [(i, len(i)) for i in lst]
