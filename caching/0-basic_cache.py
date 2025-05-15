fa2c53b2ed5867c8fe313a89ccddab6d44d4e0d7
REPLACE WITH YOUR NAME <REPLACE_WITH_YOUR_EMAIL@DOMAIN.COM>
2025-05-15 16:38:04 +0000

    added 0-basic_cache.py

---
+++ caching/0-basic_cache.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+"""
+Create a class BasicCache that inherits from BaseCaching and is a caching system
+"""
+from base_caching import BaseCaching
+
+
+class BasicCache(BaseCaching):
+    """
+    BasicCache Class
+    """
+
+    
+    def __init__(self):
+        super().__init__()
+
+    
+    def put(self, key, item):
+        """
+        put Function
+        """
+        if (key == None or item == None):
+            pass
+
+        self.cache_data[key] = item
+
+    
+    def get(self, key):
+        """
+        get Function
+        """
+        if (key == None):
+            return None
+        
+        if (key not in self.cache_data):
+            return None
+
+        return self.cache_data[key]
