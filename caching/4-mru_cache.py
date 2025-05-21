4fda37bc0508bb192b8ed741635037540df59eac
REPLACE WITH YOUR NAME <REPLACE_WITH_YOUR_EMAIL@DOMAIN.COM>
2025-05-21 06:32:39 +0000

    added 4-mru_cache.py

---
+++ caching/4-mru_cache.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+"""
+Create a class MRUCache that inherits from
+BaseCaching and is a caching system
+"""
+
+from base_caching import BaseCaching
+from collections import OrderedDict
+
+
+class MRUCache(BaseCaching):
+    """
+    LRU class
+    """
+
+    def __init__(self):
+        super().__init__()
+        self.cache_data = OrderedDict()
+
+    def put(self, key, item):
+        """
+        put method
+        """
+
+        if (key is None or item is None):
+            return
+
+        if (key in self.cache_data):
+            del self.cache_data[key]
+
+        self.cache_data[key] = item
+
+        if (len(self.cache_data) > self.MAX_ITEMS):
+            mru_key, _ = self.cache_data.popitem(last=True)
+            print("DISCARD: " + mru_key)
+
+    def get(self, key):
+        """
+        get method
+        """
+        if (key is None or key not in self.cache_data):
+            return None
+
+        self.cache_data.move_to_end(key)
+        return self.cache_data[key]
