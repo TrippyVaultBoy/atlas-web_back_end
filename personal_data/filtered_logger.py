917eadd0d8722b691c4789e7d6b5907f1518dd10
REPLACE WITH YOUR NAME <REPLACE_WITH_YOUR_EMAIL@DOMAIN.COM>
2025-05-25 22:52:01 +0000

    added annotations

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -2,10 +2,11 @@
 """
 Write a function called filter_datum that returns the log message obfuscated
 """
+from typing import List
 import re
 
 
-def filter_datum(fields, redaction, message, separator):
+def filter_datum(fields: List[str], redaction: str, message: str, separator: str) -> None:
     pattern = r'(' + '|'.join(fields) + r')=[^' + re.escape(separator) + r']*'
     masked = re.sub(pattern, lambda m: f"{m.group(1)}={redaction}", message)
     return masked
