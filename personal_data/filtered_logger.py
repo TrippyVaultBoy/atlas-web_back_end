00e5f01cb7d868283de14fd4a9f9266b29a45a2a
REPLACE WITH YOUR NAME <REPLACE_WITH_YOUR_EMAIL@DOMAIN.COM>
2025-05-25 23:48:34 +0000

    added filtered_logger.py

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -5,6 +5,10 @@ Write a function called filter_datum that returns the log message obfuscated
 from typing import List
 import re
 import logging
+import sys
+
+
+PII_FIELDS = ("name", "email", "phone", "ssn", "ip")
 
 
 class RedactingFormatter(logging.Formatter):
@@ -40,3 +44,22 @@ def filter_datum(fields: List[str],
     pattern = r'(' + '|'.join(fields) + r')=[^' + re.escape(separator) + r']*'
     masked = re.sub(pattern, lambda m: f"{m.group(1)}={redaction}", message)
     return masked
+
+
+def get_logger() -> logging.Logger:
+    """
+    get_logger function
+    """
+    logger = logging.getLogger('user_data')
+    logging.basicConfig(level=logging.INFO)
+    logger.propagate = False
+
+    logger.handlers = []
+
+    stream_handler = logging.StreamHandler(sys.stdout)
+    stream_handler.setLevel(logging.INFO)
+    stream_handler.setFormatter(RedactingFormatter(list(PII_FIELDS)))
+
+    logger.addHandler(stream_handler)
+
+    return logger
