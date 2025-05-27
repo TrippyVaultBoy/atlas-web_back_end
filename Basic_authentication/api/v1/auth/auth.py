5552f1df8a41cd8140d3b1b0285606bd08f975a6
REPLACE WITH YOUR NAME <REPLACE_WITH_YOUR_EMAIL@DOMAIN.COM>
2025-05-27 04:02:18 +0000

    added auth.py

---
+++ Basic_authentication/api/v1/auth/auth.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+"""
+create a class to manage the API authentication
+"""
+from flask import request
+from typing import List, TypeVar
+
+
+User = TypeVar('User')
+
+
+class Auth():
+    """
+    Auth class
+    """
+
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """
+        require_auth method
+        """
+        return False
+
+
+    def authorization_header(self, request=None) -> User:
+        """
+        authorization_header method
+        """
+        return None
+
+    def current_user(self, request=None) -> User:
+        """
+        current_user method
+        """
+        return None
