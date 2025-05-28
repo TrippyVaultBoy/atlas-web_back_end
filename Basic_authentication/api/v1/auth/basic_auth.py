a6ed485e094b39ecd805ff114b73dfa01e5153a6
REPLACE WITH YOUR NAME <REPLACE_WITH_YOUR_EMAIL@DOMAIN.COM>
2025-05-28 00:42:01 +0000

    added extract_base64_authorization_header method

--- Basic_authentication/api/v1/auth/basic_auth.py
+++ Basic_authentication/api/v1/auth/basic_auth.py
@@ -9,4 +9,18 @@ class BasicAuth(Auth):
     """
     BasicAuth class
     """
-    pass
+    
+
+    def extract_base64_authorization_header(self, authorization_header: str) -> str:
+        """
+        extract_base64_authorization_header method
+        """
+
+        if authorization_header is None:
+            return None
+        elif not isinstance(authorization_header, str):
+            return None
+        elif not authorization_header.startswith('Basic '):
+            return None
+        else:
+            return authorization_header.partition("Basic ")[2]
