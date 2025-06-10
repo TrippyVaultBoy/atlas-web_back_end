41afd84e8a0a63d5220888dae1e72f086c8b1a41
REPLACE WITH YOUR NAME <REPLACE_WITH_YOUR_EMAIL@DOMAIN.COM>
2025-06-10 01:05:51 +0000

    added get method

--- 0x0B_redis_basic/exercise.py
+++ 0x0B_redis_basic/exercise.py
@@ -4,7 +4,7 @@ create a Cache class and store method
 """
 import redis
 import uuid
-from typing import Union
+from typing import Union, Callable, Optional, Any
 
 
 class Cache():
@@ -25,3 +25,27 @@ class Cache():
         key = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
+
+    def get(self, key: str, fn: Optional[Callable[[bytes], Any]] = None) -> Optional[Any]:
+        """
+        get method
+        """
+
+        data = self._redis.get(key)
+        if data is None:
+            return None
+        if fn:
+            return fn(data)
+        return data
+    
+    def get_str(self, key: str) -> Optional[str]:
+        """
+        """
+
+        return self.get(key, fn=lambda d: d.decode('utf-8'))
+    
+    def get_int(self, key: str) -> Optional[int]:
+        """
+        """
+
+        return self.get(key, fn=lambda d: int(d))
