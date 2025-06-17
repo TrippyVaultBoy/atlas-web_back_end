5692862c0e17599448a1344dcfe10da2971f5f24
REPLACE WITH YOUR NAME <REPLACE_WITH_YOUR_EMAIL@DOMAIN.COM>
2025-06-17 05:21:36 +0000

    changed varaible name

--- MySQL_Advanced/3-glam_rock.sql
+++ MySQL_Advanced/3-glam_rock.sql
@@ -1,6 +1,6 @@
 -- Write a SQL script that lists all bands with Glam rock as their main style, ranked by their longevity
 SELECT band_name, style,
-    (split - formed) AS age
+    (split - formed) AS lifespan
 FROM metal_bands
 WHERE style = 'Glam rock'
-ORDER BY age DESC;
+ORDER BY lifespan DESC;
