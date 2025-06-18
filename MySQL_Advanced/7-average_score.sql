3f408e1737d00d8e22f775133372e2bf41ddbe72
REPLACE WITH YOUR NAME <REPLACE_WITH_YOUR_EMAIL@DOMAIN.COM>
2025-06-18 03:26:41 +0000

    changed variable name

--- MySQL_Advanced/7-average_score.sql
+++ MySQL_Advanced/7-average_score.sql
@@ -1,16 +1,16 @@
 -- Write a SQL script that creates a stored procedure ComputeAverageScoreForUser that computes and store the average score for a student. Note: An average score can be a decimal
 DELIMITER $$
 
-CREATE PROCEDURE ComputeAverageScoreForUser(IN user_id INT)
+CREATE PROCEDURE ComputeAverageScoreForUser(IN p_user_id INT)
 BEGIN
     DECLARE average_score FLOAT;
 
     SELECT AVG(score) INTO average_score FROM corrections
-    WHERE user_id = user_id;
+    WHERE user_id = p_user_id;
 
     UPDATE users
     SET average_score = IFNULL(average_score, 0)
-    WHERE id = user_id;
+    WHERE id = p_user_id;
 END$$
 
 DELIMITER ;
