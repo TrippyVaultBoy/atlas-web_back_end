14f734d3971ef07c39a73677495fa0d2451ce823
Tanner Saint <tsaint2004@gmail.com>
2025-06-19 04:08:34 +0000

    added 10-update_topics.py

---
+++ NoSQL/10-update_topics.py
@@ -0,0 +1,13 @@
+"""
+Write a Python function that changes all topics of a school document based on the name
+"""
+
+def update_topics(mongo_collection, name, topics):
+    """
+    update_topics function
+    """
+
+    mongo_collection.update_many(
+        { "name": name },
+        {"$set": {"topics": topics} }
+    )
