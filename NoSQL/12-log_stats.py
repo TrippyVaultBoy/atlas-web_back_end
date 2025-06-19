8c23ff292148b5d3234a1a41cb46bbd33100047d
Tanner Saint <tsaint2004@gmail.com>
2025-06-19 04:25:42 +0000

    added client connection

--- NoSQL/12-log_stats.py
+++ NoSQL/12-log_stats.py
@@ -2,8 +2,12 @@
 """
 Write a Python script that provides some stats about Nginx logs stored in MongoDB
 """
+from pymongo import MongoClient
 
 if __name__ == "__main__":
+    client = MongoClient("mongodb://127.0.0.1:27017")
+    logs = client.logs
+    
     total_logs = logs.nginx.count_documents({})
     print(f"{total_logs} logs")
 
